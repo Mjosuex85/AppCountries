293175ca39033ca6c9b7e468d7e26848804af743
Mario Vidal <mario.josuevp@gmail.com>
2023-05-24 01:42:17 +0200

    Update lineChart.js

--- lineChart.js
+++ lineChart.js
@@ -1,63 +1,18 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-
-const LineChart = () => {
-  // Obtener los datos de dates
-  const { dates } = dates;
-
-  // Obtener las fechas y los caminos únicos
-  const uniquePathsSet = new Set();
-  const labels = [];
-  const datasets = [];
-
-  dates.forEach((item) => {
-    const { date, pathList } = item;
-    labels.push(date);
-
-    pathList.forEach((pathItem) => {
-      const { path, count } = pathItem;
-
-      if (!uniquePathsSet.has(path)) {
-        uniquePathsSet.add(path);
-        datasets.push({
-          label: path,
-          data: [],
-          fill: false,
-          borderColor: getRandomColor(),
-        });
-      }
-
-      const dataIndex = Array.from(uniquePathsSet).indexOf(path);
-      datasets[dataIndex].data.push(count);
-    });
-  });
-
-  // Función para generar un color aleatorio
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
-  // Datos del gráfico
-  const data = {
-    labels: labels,
-    datasets: datasets,
-  };
-
-  // Opciones de configuración del gráfico
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-
-  return <Line data={data} options={options} />;
-};
-
-export default LineChart;
+"totalUrl": {
+                "": 1,
+                "ProduccionEnerfin": 19,
+                "CallejeroPoblaciones": 4,
+                "SIPSElectricidad": 141,
+                "ScoringScoringParticular": 1,
+                "Login": 25,
+                "callejeropoblaciones": 1,
+                "CallejeroCodigosPostales": 10,
+                "FirmaDigitalEnviarContrato": 2,
+                "CallejeroVias": 17,
+                "FirmaDigitalEstadoContrato": 8,
+                "adminStatus": 8,
+                "Callejero": 4,
+                "LoginLogin": 3,
+                "ConsumoEstimado": 51,
+                "adminEncryptPassword": 4
+            }
