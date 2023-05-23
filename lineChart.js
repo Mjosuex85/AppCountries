91ce9490b98c5bfbc0ea9c983c906ab879017a90
Mario Vidal <mario.josuevp@gmail.com>
2023-05-24 01:53:16 +0200

    Update lineChart.js

--- lineChart.js
+++ lineChart.js
@@ -1,18 +1,13 @@
-"totalUrl": {
-                "": 1,
-                "ProduccionEnerfin": 19,
-                "CallejeroPoblaciones": 4,
-                "SIPSElectricidad": 141,
-                "ScoringScoringParticular": 1,
-                "Login": 25,
-                "callejeropoblaciones": 1,
-                "CallejeroCodigosPostales": 10,
-                "FirmaDigitalEnviarContrato": 2,
-                "CallejeroVias": 17,
-                "FirmaDigitalEstadoContrato": 8,
-                "adminStatus": 8,
-                "Callejero": 4,
-                "LoginLogin": 3,
-                "ConsumoEstimado": 51,
-                "adminEncryptPassword": 4
-            }
+{
+  "_id": {
+    "$oid": "6453bcfdc664df250a3bbd0c"
+  },
+  "path": "/api/Login",
+  "method": "GET",
+  "date": {
+    "$date": "2023-05-04T14:11:09.299Z"
+  },
+  "user": "portalAuroraAtlas",
+  "body": "",
+  "params": null
+}
