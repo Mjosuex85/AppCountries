dcbe7cd15a7aca5c3d2bec8f0670b31364c26d3d
Mario Vidal <mario.josuevp@gmail.com>
2023-05-24 02:31:33 +0200

    Update lineChart.js

--- lineChart.js
+++ lineChart.js
@@ -1,13 +1,23 @@
 [
   {
     "$group": {
-      "_id": { "$dateToString": { "format": "%Y-%m-%d", "date": "$date" } },
+      "_id": {
+        "date": { "$dateToString": { "format": "%Y-%m-%d", "date": "$date" } },
+        "path": "$path"
+      },
       "count": { "$sum": 1 }
+    }
+  },
+  {
+    "$group": {
+      "_id": "$_id.date",
+      "paths": { "$push": { "path": "$_id.path", "count": "$count" } }
     }
   },
   {
     "$sort": { "_id": 1 }
   }
 ]
+
 
 
