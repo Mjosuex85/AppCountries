81983cb78c5127569628ea32ff7376e19d341807
Mario Vidal <mario.josuevp@gmail.com>
2023-05-31 21:30:50 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,187 @@
-https://sites.google.com/luzyenergiarenovable.com/intevencionesaudinfor
-
-
-live_OiNNHrffJCaCn5E4JfY1zGonTraucDeFlaTfnsVJkzSGWVcTSNa0p4NlB1GMOVnD
+"dates": [
+            {
+                "pathList": [
+                    {
+                        "count": 1,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/CodigosPostales"
+                    },
+                    {
+                        "count": 5,
+                        "method": "GET",
+                        "path": "/api/Login/"
+                    },
+                    {
+                        "count": 2,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/Vias"
+                    },
+                    {
+                        "count": 2,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/Poblaciones"
+                    },
+                    {
+                        "count": 3,
+                        "method": "GET",
+                        "path": "/api/v1/SIPSElectricidad"
+                    },
+                    {
+                        "count": 7,
+                        "method": "GET",
+                        "path": "/api/v1/Status"
+                    },
+                    {
+                        "count": 1,
+                        "method": "GET",
+                        "path": "/api/v1/ConsumoEstimado"
+                    }
+                ],
+                "date": "2023-05-08"
+            },
+            {
+                "pathList": [
+                    {
+                        "count": 2,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/CodigosPostales"
+                    },
+                    {
+                        "count": 34,
+                        "method": "OPTIONS",
+                        "path": "/api/v1/Status"
+                    },
+                    {
+                        "count": 2,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/Vias"
+                    },
+                    {
+                        "count": 1,
+                        "method": "GET",
+                        "path": "/api/v1/ConsumoEstimado"
+                    },
+                    {
+                        "count": 9,
+                        "method": "GET",
+                        "path": "/api/v1/Status"
+                    },
+                    {
+                        "count": 1,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/Poblaciones"
+                    },
+                    {
+                        "count": 1,
+                        "method": "GET",
+                        "path": "/api/v1/SIPSElectricidad"
+                    }
+                ],
+                "date": "2023-05-10"
+            },
+            {
+                "pathList": [
+                    {
+                        "count": 2,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/CodigosPostales"
+                    },
+                    {
+                        "count": 2,
+                        "method": "GET",
+                        "path": "/api/v1/ConsumoEstimado"
+                    },
+                    {
+                        "count": 3,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/Vias"
+                    },
+                    {
+                        "count": 3,
+                        "method": "GET",
+                        "path": "/api/Login/"
+                    },
+                    {
+                        "count": 4,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/Poblaciones"
+                    },
+                    {
+                        "count": 7,
+                        "method": "GET",
+                        "path": "/api/v1/SIPSElectricidad"
+                    }
+                ],
+                "date": "2023-05-11"
+            },
+            {
+                "pathList": [
+                    {
+                        "count": 2,
+                        "method": "GET",
+                        "path": "/api/v1/ConsumoEstimado"
+                    },
+                    {
+                        "count": 1,
+                        "method": "GET",
+                        "path": "/api/admin/Status"
+                    },
+                    {
+                        "count": 4,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/Vias"
+                    },
+                    {
+                        "count": 4,
+                        "method": "GET",
+                        "path": "/api/Login/"
+                    },
+                    {
+                        "count": 2,
+                        "method": "GET",
+                        "path": "/api/v1/SIPSElectricidad"
+                    },
+                    {
+                        "count": 4,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/CodigosPostales"
+                    },
+                    {
+                        "count": 4,
+                        "method": "POST",
+                        "path": "/api/v1/Callejero/Poblaciones"
+                    }
+                ],
+                "date": "2023-05-12"
+            },
+            {
+                "pathList": [
+                    {
+                        "count": 35,
+                        "method": "OPTIONS",
+                        "path": "/api/v1/Status"
+                    },
+                    {
+                        "count": 9,
+                        "method": "GET",
+                        "path": "/api/admin/Status"
+                    },
+                    {
+                        "count": 2,
+                        "method": "GET",
+                        "path": "/api/v1/ConsumoEstimado"
+                    },
+                    {
+                        "count": 1,
+                        "method": "GET",
+                        "path": "/api/Login/"
+                    },
+                    {
+                        "count": 3,
+                        "method": "GET",
+                        "path": "/api/v1/Status"
+                    }
+                ],
+                "date": "2023-05-16"
+            }
+        ]
