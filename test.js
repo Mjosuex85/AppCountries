e6028e89a65da87587706d9ec8de7bb08cad30d6
Mario Vidal <mario.josuevp@gmail.com>
2023-06-03 01:30:55 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,18 @@
- const info = useMemo(
-    function () {
+const info = useMemo(() => {
+  return {
+    labels,
+    datasets: result.map((item, index) => {
       return {
-        labels,
-        datasets: [
-          {
-      
-            label: lines[0]?.path,
-            data: lines[0]?.counts,
-            transition: 0.1,
-            tension: 0.1,
-            borderColor: "FF5733",
-            borderRadius: "30px",
-            pointBackgroundColor: "FF5733",
-            borderWidth: 0.9,
-          }
-        ],
+        label: item.path,
+        data: item.counts,
+        transition: 0.1,
+        tension: 0.1,
+        borderColor: "FF5733",
+        borderRadius: "30px",
+        pointBackgroundColor: "FF5733",
+        borderWidth: 0.9,
       };
-    },
-    [labels]
-  );
+    }),
+  };
+}, [labels]);
+
