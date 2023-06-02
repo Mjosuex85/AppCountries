2e62142865533d69ed28273852812e608670e828
Mario Vidal <mario.josuevp@gmail.com>
2023-06-03 01:33:26 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,23 @@
+// Función para generar un color aleatorio en formato hexadecimal
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 const info = useMemo(() => {
   return {
     labels,
     datasets: result.map((item, index) => {
+      const color = randomColor();
       return {
         label: item.path,
         data: item.counts,
         transition: 0.1,
         tension: 0.1,
-        borderColor: "FF5733",
+        borderColor: color,
         borderRadius: "30px",
-        pointBackgroundColor: "FF5733",
+        pointBackgroundColor: color,
         borderWidth: 0.9,
       };
     }),
   };
 }, [labels]);
-
