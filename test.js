ac3d19edd9effd980d4a8b49be82e45e4ab5fcaa
Mario Vidal <mario.josuevp@gmail.com>
2023-06-05 20:51:09 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,95 +1,128 @@
-import rootReducer from "./reducer";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { useEffect, useMemo, useState } from "react";
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+import style from "./lineChart.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getDates } from "../../../redux/actions";
 
-const store = createStore(
-  rootReducer,(applyMiddleware(thunk))
-);
+Chart.register(...registerables);
 
-export default store;
-
-
-
-
-import "./App.css";
-import { Dashboard } from "./components/dashBoard/Dashboard";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { getAllInfo, getDates, setMonthDays } from "./redux/actions";
-import { useState } from "react";
-
-function App() {
+export default function LineChart(data) {
   const dispatch = useDispatch();
-  const dates = useSelector((state) => state.dates)
-  const data = useSelector((state) => state.allInfo);
-  const [date, setDate] = useState("track_202305");
-  
-  console.log("la primera fecha, luego cambia",date)
 
-  const handleChange = (event) => {
-    event.preventDefault();
-    setDate(event.target.value);
-  };
+  const fechas = useSelector((state) => state.allMonthDays);
+  const [labels, setLabels] = useState([]);
+  const [lines, setLines] = useState([]);
+
+  useEffect(() => {
+    let pathCounts = {};
+
+    data.data?.forEach((date) => {
+      date.pathList.forEach((pathObj) => {
+        const { path, count } = pathObj;
+
+        if (!pathCounts[path]) {
+          pathCounts[path] = {};
+        }
+
+        pathCounts[path][date.date] = count;
+      });
+    });
+
+    let result = [];
+
+    for (let path in pathCounts) {
+      let counts = [];
+
+      fechas.forEach((fecha) => {
+        const count = pathCounts[path][fecha] || 0;
+        counts.push(count);
+      });
+
+      result.push({path: path, counts: counts });
+    }
+
+    setLines(result);
+  }, []);
+
+
+  useEffect(() => {
+    if (Object.entries(fechas).length === 0) {
+      console.log("setea fechas si está vacio");
+    } else {
+      setLabels(fechas);
+    }
+  }, [fechas]);
 
 
   useEffect(() => {
     dispatch(getDates());
-    dispatch(getAllInfo(date));
-  }, []);
-  
-  useEffect(() => {
-    dispatch(getAllInfo(date))
-    dispatch(setMonthDays(date))
-  },[date]) 
+  }, [labels]);
+
+  const options = {
+    fill: false,
+    responsive: true,
+    scales: {
+      y: {
+        min: 0,
+      },
+    },
+  };
+
+ /*  console.log("TRAE ESTO",lines.map((e) => e.counts)); */
+
+  function generateRandomColors(count) {
+    const colors = [];
+    const availableColors = [
+      "#FF5733 ",
+      "#33FF57",
+      "#5733FF",
+      "#FF33E6",
+      "#33E6FF",
+      "#E6FF33",
+    ];
+
+    for (let i = 0; i < count; i++) {
+      const randomIndex = Math.floor(Math.random() * availableColors.length);
+      const color = availableColors[randomIndex];
+      availableColors.splice(randomIndex, 1);
+      colors.push(color);
+    }
+
+    return colors;
+  }
+
+  const info = useMemo(() => {
+    const colors = generateRandomColors(lines.length);
+
+    return {
+      labels,
+      datasets: lines.map((item, index) => {
+        const color = colors[index];
+        return {
+          id: index,
+          label: item.path,
+          data: item.counts,
+          transition: 0.1,
+          tension: 0.1,
+          borderColor: color,
+          borderRadius: "30px",
+          pointBackgroundColor: color,
+          borderWidth: 0.9,
+        };
+      }),
+    };
+  }, [labels]);
+
+console.log("esta info",info)
 
 
-  return (
-    <>  
-    <div style={{marginLeft: '2px', marginBottom: '20px'}}>
-      <select style={{borderRadius: '20px', paddingLeft: '20px', paddingRight: '20px'}} onChange={(e) => handleChange(e)}>
-        <option> Selecionar Fecha...</option>
-        {dates.data?.dates.map((e, index) => {
-          return <option key={index}> {e} </option>;
-        })}
-      </select>
-    </div> 
-
-        <Dashboard apiData={data} />
- 
-    </>
-  );
-}
-
-export default App; 
-
-
-
-
-
-
-
-
-
-import LastTen from "../lastTen/LastTen";
-import BarChart from "../charts/barChart/BarChart";
-import Users from "../general/totalUsers/Users";
-import LineChart from "../charts/lineChart/LineChart";
-import style from './dashboard.module.css';
-
-
-export const Dashboard = (apiData) => {
   return (
     <div className={style.container}>
-      <div className={style.cards}>
-        <LineChart data={apiData.apiData.data?.dates} />
-        <BarChart data={apiData.apiData.data?.pathCount}/>
-      </div>
-
-      <div className={style.cards}>
-        <LastTen data={apiData.apiData.data?.lastTen} />
-        <Users data={apiData.apiData.data?.users} />
+      <div>
+        <Line datasetIdKey="id" data={info} options={options} />
       </div>
     </div>
   );
-};
+}
 
