abcd8bd28b046d3c80330f629c4efbbd791a4b47
Mario Vidal <mario.josuevp@gmail.com>
2023-06-09 11:38:31 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,41 @@
-https://meet.google.com/qmr-xkwx-ejg
+  <div class="barra">
+        <div class="texto-animado">
+          <span>Texto desplazándose</span>
+          <span>Texto 2</span>
+          <span>Texto 3</span>
+          <span>Texto 4</span>
+        </div>
+      </div>
 
+// CSS
 
+.barra {
+    width: 30%;
+    height: 50px;
+    background-color: #ccc;
+    overflow: hidden;
+    position: relative;
+  }
+  
+  .texto-animado {
+    position: relative;
+    white-space: nowrap;
+    animation: desplazamiento 8s linear infinite;
+    opacity: 0;
+  }
+  
+  @keyframes desplazamiento {
+    0% {
+      transform: translateX(100%);
+      opacity: 0;
+    }
+    10% {
+      opacity: 1;
+    }
+    100% {
+      transform: translateX(-100%);
+      opacity: 1;
+    }
+  }
+  
+  
