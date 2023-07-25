3b9068df5b32541bdbce51e68a11d629ae48c441
Mario Vidal <mario.josuevp@gmail.com>
2023-07-25 22:42:04 +0200

    Update prueba.cs

--- prueba.cs
+++ prueba.cs
@@ -1,1 +1,10 @@
+protected void GetGenericFields(BasePriceIndexed price)
+        {
 
+            Console.WriteLine(price.CG.ToString(SixDecimalFormat, Culture));
+            Fields["duracion"] = price.Duration.Name;
+            Fields["cg"]       = price.CG.ToString(SixDecimalFormat, Culture);
+            Fields["cf"]       = price.CF.ToString(SixDecimalFormat, Culture);
+            Fields["gdo"]      = price.GDO.ToString(SixDecimalFormat, Culture);
+            Fields["ifnee"]    = price.IFNEE.ToString(SixDecimalFormat, Culture);  
+        }
