239f484ea12c33cd56d9e92e688779605e6733d3
Mario Vidal <mario.josuevp@gmail.com>
2023-07-25 23:41:59 +0200

    Update prueba.cs

--- prueba.cs
+++ prueba.cs
@@ -1,4 +1,4 @@
-f (string.IsNullOrEmpty(input))
+if (string.IsNullOrEmpty(input))
             return input;
 
         int length = input.Length;
@@ -13,8 +13,13 @@ f (string.IsNullOrEmpty(input))
         // If we found a non-zero character, check if it's a decimal separator.
         if (index >= 0 && input[index] == ',')
         {
-            // If it is, we need to keep one trailing zero after the decimal separator.
-            index++;
+            // If it is, we need to keep two trailing zeros after the decimal separator.
+            index += 3;
+        }
+        else if (index >= 0)
+        {
+            // If it's a non-zero digit before the decimal separator, we need to keep one trailing zero.
+            index += 2;
         }
 
         return input.Substring(0, index + 1);
