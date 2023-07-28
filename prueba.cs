8a1e874ad75c8304f494ea097072d5ea1e2ff9e4
Mario Vidal <mario.josuevp@gmail.com>
2023-07-28 04:30:35 +0200

    Update prueba.cs

--- prueba.cs
+++ prueba.cs
@@ -1,11 +1,1 @@
-function parseDateAndFormat(dateStr) {
-  // Convertir la cadena a un objeto de fecha
-  const dateObj = new Date(dateStr);
-  // Obtener el nombre del mes en formato abreviado (MMM)
-  const month = dateObj.toLocaleString('default', { month: 'short' });
-  // Obtener el año en formato YYYY
-  const year = dateObj.getFullYear();
-  // Formatear la fecha como "MMM-YYYY" (por ejemplo, "Jul-2023")
-  const formattedDate = `${month}-${year}`;
-  return formattedDate;
-}
+{{ news[data.field] | date: 'MMM-YYYY' }}
