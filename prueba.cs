34de66d13c7ef106b4bd887ce6d2c8852b8e2a79
Mario Vidal <mario.josuevp@gmail.com>
2023-07-28 04:43:45 +0200

    Update prueba.cs

--- prueba.cs
+++ prueba.cs
@@ -1,45 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ReleaseNew } from '../../models/release-new.model';
+  ngOnInit(): void {
 
-@Component({
-    selector: 'app-release-news-table',
-    templateUrl: './release-news-table.component.html',
-    styleUrls: ['./release-news-table.component.scss']
-})
-export class ReleaseNewsTableComponent implements OnInit {
-
-    @Input() labels      : any;
-    @Input() showCustomer: boolean = true;
-    @Input() releaseNews : ReleaseNew[];
-
-    public tableColumns = [
-        { header: 'Disponibilidad', type: "date", format: "date"   , field: 'creationDate'   },
-        { header: 'Novedad'       , type: "text", format: "default", field: 'description' }
-    ];
-
-    public globalFilterFields: string[] = [ 'description' ];
-
-    ngOnInit(): void {
-
-        this.releaseNews.map((release) => {
+        this.releaseNews = this.releaseNews.map((release) => {
             release['disabled'] = false;
-           console.log(release.creationDate.)
-          /*   console.log("----------",release.creationDate) */
-            return true;
-        })
-
-    }
-
-    private parseDate = (date: Date) => {
-        const dateObj = new Date(date);
-        // Obtener el nombre del mes en formato abreviado (MMM)
-        const month = dateObj.toLocaleString('default', { month: 'short' });
-        // Obtener el año en formato YYYY
-        const year = dateObj.getFullYear();
-        // Formatear la fecha como "MMM-YYYY" (por ejemplo, "Jul-2023")
-        const formattedDate = `${month}-${year}`;
-        console.log(formattedDate)
-        
-    }
-
-}
+            // Convertir la cadena a un objeto Date
+            const dateObject = new Date(release.creationDate);
+            // Formatear la fecha en formato JUL-2023
+            release.creationDate = formatDate(dateObject, 'MMM-yyyy', 'en');
+            return release;
