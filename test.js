694c4018a24b6e93662c63dc7609b982abc7d94f
Mario Vidal <mario.josuevp@gmail.com>
2023-08-21 20:42:24 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,30 +1,10 @@
-try
-    {
-        // Obtén la oferta específica que deseas actualizar
-        var dbOffer = await Repository.FindAsync<OfferRegular>(offerId);
+   public resendDigitalSignature(offerId: number) {
         
-        if (dbOffer != null)
-        {
-            dbOffer.State = (int)offerStateId;
-
-            // Actualiza la oferta
-            await Repository.Update<OfferRegular>(dbOffer);
-            bool dbUpdateErrorResult = await Repository.Commit();
-            
-            if (!dbUpdateErrorResult)
-            {
-                Logger.LogError($"Error al actualizar el estado de la oferta {dbOffer.Id}...");
+        
+        this.offerRegularService.resendDigitalSignature(offerId).subscribe({
+            next: (data: boolean) => {
+                
+                console.log(data)
             }
-        }
-        else
-        {
-            Logger.LogError($"No se encontró una oferta con el ID {offerId}...");
-        }
-
-        return new RequestPipelineResult<bool>(true);
-    }
-    catch (Exception ex)
-    {
-        Logger.LogError($"Error en UpdateContractStateOffer: {ex.Message}");
-        return new RequestPipelineResult<bool>(false);
+        })
     }
