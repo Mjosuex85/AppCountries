fd10b63fa7f63cd1c6d0843321b40eb867696a6d
Mario Vidal <mario.josuevp@gmail.com>
2023-08-22 12:03:17 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,24 +1,22 @@
-import { Component } from '@angular/core';
+  <button mat-flat-button *ngIf="offer.stateId === 9 && checkPermissions()" [disabled]="offer.disabledButton" [title]="labels.resendSingaturButton" 
+                    class="button-complementary-color-one rounded-0  me-0 me-lg-2 mb-2 mb-lg-0"
+                    (click)="openConfirmationDialog(offer.id, rowIndex)">
 
-@Component({
-  selector: 'app-tu-componente',
-  templateUrl: './tu-componente.component.html',
-})
-export class TuComponente {
-  displayConfirmationDialog: boolean = false;
+                    <p-dialog [(visible)]="displayConfirmationDialog" header="Confirmación"  [style]="{ width: '300px' }">
+                        <p>
+                            ¿Está seguro de que desea reenviar la firma digital?
+                        </p>
+                        <p-footer>
+                            <button type="button" pButton label="Sí" (click)="openConfirmationDialog(offer.id, rowIndex)" class="p-button-success"></button>
+                            <button type="button" pButton label="No" (click)="openConfirmationDialog(offer.id, rowIndex)" class="p-button-secondary"></button>
+                        </p-footer>
+                    </p-dialog>
 
-  // Resto del código de tu componente...
 
-  openConfirmationDialog(offerId: number, index: number): void {
-    this.displayConfirmationDialog = true;
-  }
-
-  confirm(): void {
-    // Aquí puedes realizar la acción de confirmación, como llamar a resendDigitalSignature
-    this.displayConfirmationDialog = false;
-  }
-
-  cancel(): void {
-    this.displayConfirmationDialog = false;
-  }
-}
+                    <div *ngIf="!offer.disabledButton" class="d-flex flex-row justify-content-center align-items-center">
+                        <fa-icon [icon]="['fas', 'rotate-right']"></fa-icon>
+                    </div>
+                    <div *ngIf="offer.disabledButton" class="d-flex flex-row justify-content-center align-items-center">
+                        <fa-icon [icon]="['fas', 'spinner']" [spin]="true" class="me-2"></fa-icon>
+                    </div>
+                </button>
