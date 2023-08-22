677d6e4f90e6a43c94fcc7d1b6ba7d32a73c3c0f
Mario Vidal <mario.josuevp@gmail.com>
2023-08-22 11:55:09 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,43 +1,9 @@
-<button mat-flat-button *ngIf="offer.stateId === 9 && checkPermissions()" [disabled]="offer.disabledButton" [title]="labels.resendSingaturButton" 
-                    class="button-complementary-color-one rounded-0  me-0 me-lg-2 mb-2 mb-lg-0"
-                    (click)="openConfirmationDialog(offer.id, rowIndex)">
-                    <div *ngIf="!offer.disabledButton" class="d-flex flex-row justify-content-center align-items-center">
-                        <fa-icon [icon]="['fas', 'rotate-right']"></fa-icon>
-                    </div>
-                    <div *ngIf="offer.disabledButton" class="d-flex flex-row justify-content-center align-items-center">
-                        <fa-icon [icon]="['fas', 'spinner']" [spin]="true" class="me-2"></fa-icon>
-                    </div>
-</button>
-
-
-public resendDigitalSignature(offerId: number, index: number) {
-        
-        this.isResendingSignature = true
-        this.offers[index]['disabledButton'] = true;
-
-        this.offerRegularService.resendDigitalSignature(offerId).subscribe({
-            next: (data: boolean) => {
-                
-                console.log(data)
-                
-            }
-        }).add(() => {
-            this.isResendingSignature = false;
-            this.offers[index]['disabledButton'] = false;
-        });
-
-    }
-
-
-    public openConfirmationDialog(offerId: number, index: number) : void {
-
-        this.confirmationService.confirm({
-            message: "Se reenviará el contrato a firma digital",
-            accept: () => {
-    
-                this.resendDigitalSignature(offerId, index)
-                
-            }
-        })
-
-    }
+<p-dialog [(visible)]="displayConfirmationDialog" header="Confirmación" modal="true" [style]="{ width: '300px' }">
+    <p>
+        ¿Está seguro de que desea reenviar la firma digital?
+    </p>
+    <p-footer>
+        <button type="button" pButton label="Sí" (click)="confirm()" class="p-button-success"></button>
+        <button type="button" pButton label="No" (click)="cancel()" class="p-button-secondary"></button>
+    </p-footer>
+</p-dialog>
