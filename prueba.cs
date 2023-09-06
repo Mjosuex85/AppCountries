e1db3d95b7955bd8f971b19057263acc0f77408f
Mario Vidal <mario.josuevp@gmail.com>
2023-09-07 00:23:07 +0200

    Update prueba.cs

--- prueba.cs
+++ prueba.cs
@@ -1,9 +1,2 @@
-  ngOnInit(): void {
+  
 
-        this.releaseNews = this.releaseNews.map((release) => {
-            release['disabled'] = false;
-            // Convertir la cadena a un objeto Date
-            const dateObject = new Date(release.creationDate);
-            // Formatear la fecha en formato JUL-2023
-            release.creationDate = formatDate(dateObject, 'MMM-yyyy', 'en');
-            return release;
